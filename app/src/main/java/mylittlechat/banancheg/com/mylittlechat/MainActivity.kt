dab1ccc6a8ddd13addbecce5025cc4c2690f4f3f
Ruslan <ruslansolodovnyk@gmail.com>
2018-11-13 19:30:18 +0200

    Add custom Adapter, code MainActyvity

--- app/src/main/java/mylittlechat/banancheg/com/mylittlechat/MainActivity.kt
+++ app/src/main/java/mylittlechat/banancheg/com/mylittlechat/MainActivity.kt
@@ -2,12 +2,38 @@ package mylittlechat.banancheg.com.mylittlechat
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.message_user_one.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+
+    private val myAdapter: MyAdapter = MyAdapter();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView.adapter = myAdapter
+        recyclerView.addItemDecoration(MyItemDecorator())
+        buttonOk.setOnClickListener(this)
     }
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.buttonOk ->{
+                if(radioGroup.checkedRadioButtonId == R.id.radioBtnUserOne){
+                    val userMessage = UserOneMessage(editText.text.toString())
+                    myAdapter.addView(userMessage)
+                }else if(radioGroup.checkedRadioButtonId == R.id.radioBtnUserTwo){
+                    val userMessage = UserTwoMessage(editText.text.toString())
+                    myAdapter.addView(userMessage)
+                }
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/mylittlechat/banancheg/com/mylittlechat/MyAdapter.kt
+++ app/src/main/java/mylittlechat/banancheg/com/mylittlechat/MyAdapter.kt
@@ -12,34 +12,50 @@ import android.Manifest.permission_group.SMS
 import android.telecom.Call
 
 
-class MyAdapter : RecyclerView.Adapter<MyAdapter.ViewHolder>(){
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+class MyAdapter (): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+
+
+    val TYPE_HEADER = 0
+    val TYPE_FIRST_USER_MESSAGE = 1
+    val TYPE_SECOND_USER_MESSAGE = 2
+
+    private val messagesList: MutableList<UserMessage> = ArrayList()
+    private var firstUserMessageCounter: Int =0
+    private var secondUserMessageCounter: Int = 0
+
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val layout: Int = getItemLayout(viewType)
         val view: View = LayoutInflater.from(viewGroup.context).inflate(layout, viewGroup, false)
-        return ViewHolder(view)
+        when(viewType){
+            TYPE_HEADER -> return HeaderViewHolder(view)
+            TYPE_SECOND_USER_MESSAGE or TYPE_FIRST_USER_MESSAGE -> return MessageViewHolder(view)
+            else -> throw IllegalArgumentException()
+        }
+
     }
 
     override fun getItemCount(): Int {
         return messagesList.size
     }
 
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        if(position == TYPE_HEADER){
-            viewHolder.bindHeader(firstUserMessageCounter, secondUserMessageCounter)
-        }else{
-            val message = messagesList.get(position).message
-            viewHolder.bind(message)
+    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
+        when(viewHolder.itemViewType){
+            TYPE_HEADER ->{
+                var viewHolder0: HeaderViewHolder = viewHolder  as HeaderViewHolder
+                viewHolder0.firstUserMessageCounter.setText(R.string.user_one_count)
+                viewHolder0.secondUserMessageCounter.setText(R.string.user_two_count)
+            }
+            TYPE_SECOND_USER_MESSAGE or TYPE_FIRST_USER_MESSAGE -> {
+                var viewHolder0: MessageViewHolder = viewHolder  as MessageViewHolder
+                viewHolder0.messageTextView.setText(messagesList.get(position).message)
+            }
+
         }
     }
 
-     private var firstUserMessageCounter: Int =0
-     private var secondUserMessageCounter: Int = 0
 
-    val TYPE_HEADER = 0
-    val TYPE_FIRST_USER_MESSAGE = 1
-    val TYPE_SECOND_USER_MESSAGE = 2
-
-    var messagesList: ArrayList<UserMessage> = ArrayList()
 
 
     fun getItemLayout(viewType: Int): Int {
@@ -63,7 +79,7 @@ class MyAdapter : RecyclerView.Adapter<MyAdapter.ViewHolder>(){
         return -1
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    /*inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         fun bind(message: String?) {
             itemView.findViewById<TextView>(R.id.messageTextView).text = message
@@ -73,6 +89,31 @@ class MyAdapter : RecyclerView.Adapter<MyAdapter.ViewHolder>(){
             itemView.findViewById<TextView>(R.id.count_user_one).text = firstUserMessageCounter.toString()
             itemView.findViewById<TextView>(R.id.count_user_two).text = secondUserMessageCounter.toString()
         }
+    }*/
+    inner class MessageViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+
+        var messageTextView: TextView
+
+        init {
+            messageTextView = v.findViewById(R.id.messageTextView)
+        }
+    }
+
+    inner class HeaderViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+
+        var firstUserMessageCounter: TextView
+        var secondUserMessageCounter: TextView
+
+        init {
+            firstUserMessageCounter = v.findViewById(R.id.count_user_one)
+            secondUserMessageCounter = v.findViewById(R.id.count_user_two)
+        }
+    }
+
+    fun addView(message: UserMessage) {
+        messagesList.add(message)
+        notifyItemChanged(0)
+        notifyItemInserted(messagesList.size)
     }
 
 
